e6ea1bec974fd6a2eb76f538e25668928f5aaa32
GGCORDEIRO <103956367+GGCORDEIRO@users.noreply.github.com>
2022-06-12 17:06:37 -0300

    Create pdf.php

---
+++ pages/pdf.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- Descrição-->
+    <meta name="author" content="Meu Portfólio">
+    <meta name="description" content="Projeto para mostrar desempenho de Geovane">
+    
+    <!--Favicon-->
+    <link rel="icon" type="image/x-icon" href="/src/assets/icone.png">
+    
+    <!--CSS-->
+    <link rel="stylesheet" type="text/css" href="/src/css/style.css">
+    <link rel="stylesheet" type="text/css" href="/src/css/page.css">
+    
+    <!--Titulo-->
+    <title>Meu Portfólio</title>
+  </head>
+  <body>
+
+        <!--Topo da Página-->
+        <?php require_once('header.php'); ?>
+
+        <!--Meio da Página-->
+        <main class="conteudo-contato">
+          <object data="/src/assets/Curriculum_Geo.pdf" height="300%" width="80%" style="margin-top: 40px;"></object>
+        </main>
+    
+    <!--Fim da Página-->
+<?php require_once('footer.php'); ?>
+
+  </body>
+</html>
